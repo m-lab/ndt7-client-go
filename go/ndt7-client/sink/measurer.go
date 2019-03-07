d89008427f27cb1d9246ff6795072baaf369cbac
Simone Basso <bassosimone@gmail.com>
2019-03-07 19:29:42 +0000

    Add nonstandard speed field for convenience

--- go/ndt7-client/sink/measurer.go
+++ go/ndt7-client/sink/measurer.go
@@ -9,6 +9,9 @@ import (
 
 type appinfo struct {
 	NumBytes int64 `json:"num_bytes"`
+	// Speed is not part of the spec but it helps to see it on the
+	// wire when we're not using BBR-enabled ndt7
+	Speed float64 `json:"speed"`
 }
 
 type measurement struct {
@@ -46,6 +49,7 @@ func Measurer(in <-chan int64) <-chan []byte {
 			var m measurement
 			m.Elapsed = float64(t.Sub(begin)) / float64(time.Second)
 			m.AppInfo.NumBytes = total
+			m.AppInfo.Speed = 8.0 * float64(total) / m.Elapsed / 1000.0 / 1000.0
 			data, err := json.Marshal(m)
 			if err != nil {
 				return
