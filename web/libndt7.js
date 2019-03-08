a1b3bbd6e85c6632ba7e88662fcfd07a4a4c27dd
Simone Basso <bassosimone@gmail.com>
2019-03-08 16:39:57 +0000

    Finish up JavaScript client

--- web/libndt7.js
+++ web/libndt7.js
@@ -3,7 +3,7 @@
 
 // libndt7 is a ndt7 client library in JavaScript.
 const libndt7 = (function () {
-  'use strict';
+  'use strict'
 
   // events groups all events
   const events = {
@@ -69,13 +69,16 @@ const libndt7 = (function () {
       }
 
       // setupconn creates the WebSocket connection and initializes all
-      // the event handlers except for the message handler. To setup the
-      // WebSocket connection we use the |settings| and the |subtest|.
+      // the event handlers except for the message handler and, when
+      // uploading, the connect handler. To setup the WebSocket connection
+      // we use the |settings| and the |subtest| arguments.
       const setupconn = function (settings, subtest) {
         const url = makeurl(settings, subtest)
         const socket = new WebSocket(url, 'net.measurementlab.ndt.v7')
-        socket.onopen = function (event) {
-          emit(events.open, null)
+        if (subtest === 'download') {
+          socket.onopen = function (event) {
+            emit(events.open, null)
+          }
         }
         socket.onclose = function (event) {
           emit(events.close, null)
@@ -86,9 +89,9 @@ const libndt7 = (function () {
         return socket
       }
 
-      // measure measures the performance using |socket|. To this end, it
-      // sets the message handler of the WebSocket |socket|.
-      const measure = function (socket) {
+      // download measures the download speed using |socket|. To this end, it
+      // sets the message handlers of |socket|.
+      const download = function (socket) {
         let count = 0
         const t0 = new Date().getTime()
         let tlast = t0
@@ -113,6 +116,49 @@ const libndt7 = (function () {
         }
       }
 
+      // uploader performs the read upload.
+      const uploader = function (socket, data, t0, tlast, count) {
+        let t1 = new Date().getTime()
+        const duration = 10000  // millisecond
+        if (t1 - t0 > duration) {
+          return
+        }
+        if (socket.bufferedAmount < data.length) {
+          socket.send(data)
+          count += data.length
+        }
+        const every = 250  // millisecond
+        if (t1 - tlast > every) {
+          emit(events.clientMeasurement, {
+            elapsed: (t1 - t0) / 1000,  // second
+            app_info: {
+              num_bytes: count
+            }
+          })
+          tlast = t1
+        }
+        setTimeout(function() {
+          uploader(socket, data, t0, tlast, count)
+        }, 0)
+      }
+
+      // upload measures the upload speed using |socket|. To this end, it
+      // sets the message and open handlers of |socket|.
+      const upload = function (socket) {
+        socket.onmessage = function (event) {
+          emit(events.serverMeasurement, JSON.parse(event.data))
+        }
+        socket.onopen = function (event) {
+          emit(events.open, null)
+          const data = new Uint8Array(1 << 13)
+          crypto.getRandomValues(data)
+          socket.binarytype = 'arraybuffer'
+          const t0 = new Date().getTime()
+          const tlast = t0
+          uploader(socket, data, t0, tlast, 0)
+        }
+      }
+
       return {
         // on is a publicly exported function that allows to set a handler
         // for a specific event emitted by this library. |key| is the handler
@@ -123,7 +169,12 @@ const libndt7 = (function () {
 
         // startDownload starts the ndt7 download.
         startDownload: function () {
-          measure(setupconn(settings, 'download'))
+          download(setupconn(settings, 'download'))
+        },
+
+        // startUpload starts the ndt7 upload.
+        startUpload: function () {
+          upload(setupconn(settings, 'upload'))
         }
       }
     }
