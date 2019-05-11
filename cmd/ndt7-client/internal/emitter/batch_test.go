060f0484f5443d8127d1ab5fa8438ed44ecd5036
Simone Basso <bassosimone@gmail.com>
2019-05-11 19:08:36 +0200

    Write assertive tests for batch

--- cmd/ndt7-client/internal/emitter/batch_test.go
+++ cmd/ndt7-client/internal/emitter/batch_test.go
@@ -3,71 +3,332 @@ package emitter
 import (
 	"encoding/json"
 	"errors"
+	"os"
 	"testing"
 
 	"github.com/m-lab/ndt7-client-go/cmd/ndt7-client/internal/mocks"
 	"github.com/m-lab/ndt7-client-go/spec"
 )
 
-// TestBatchDownloadError is what happens if you
-// run a download and it results in an error.
-func TestBatchDownloadError(t *testing.T) {
-	batch := NewBatch()
+// TestBatchOnStarting verifies that OnStarting works correctly
+func TestBatchOnStarting(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
 	err := batch.OnStarting("download")
 	if err != nil {
-		t.Fatal("Did not expect an error here")
+		t.Fatal(err)
 	}
-	err = batch.OnError("download", errors.New("mocked error"))
-	if err != nil {
-		t.Fatal("Did not expect an error here")
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
 	}
-	err = batch.OnComplete("download")
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			Subtest string `json:"subtest"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
 	if err != nil {
-		t.Fatal("Did not expect an error here")
+		t.Fatal(err)
+	}
+	if event.Key != "status.measurement_start" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.Subtest != "download" {
+		t.Fatal("Unexpected subtest field value")
 	}
 }
 
-// TestBatchDownloadNormal is what happens if you
-// run a download and it there are no errors.
-func TestBatchDownloadNormal(t *testing.T) {
-	batch := NewBatch()
+// TestBatchOnStartingFailure verifies that OnStarting
+// fails if we cannot write.
+func TestBatchOnStartingFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
 	err := batch.OnStarting("download")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnConnected("download", "FQDN")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnDownloadEvent(&spec.Measurement{})
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnComplete("download")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
 	}
 }
 
-// TestBatchUploadNormal is what happens
-// during a normal upload subtest.
-func TestBatchUploadNormal(t *testing.T) {
+// TestBatchOnError verifies that OnError works correctly
+func TestBatchOnError(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
+	err := batch.OnError("download", errors.New("mocked error"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
+	}
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			Failure string `json:"failure"`
+			Subtest string `json:"subtest"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Key != "failure.measurement" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.Subtest != "download" {
+		t.Fatal("Unexpected subtest field value")
+	}
+	if event.Value.Failure != "mocked error" {
+		t.Fatal("Unexpected failure field value")
+	}
+}
+
+// TestBatchOnErrorFailure verifies that OnError
+// fails if we cannot write.
+func TestBatchOnErrorFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
+	err := batch.OnError("download", errors.New("some error"))
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
+	}
+}
+
+// TestBatchOnConnected verifies that OnConnected works correctly
+func TestBatchOnConnected(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
+	err := batch.OnConnected("download", "FQDN")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
+	}
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			Server  string `json:"server"`
+			Subtest string `json:"subtest"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Key != "status.measurement_begin" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.Subtest != "download" {
+		t.Fatal("Unexpected subtest field value")
+	}
+	if event.Value.Server != "FQDN" {
+		t.Fatal("Unexpected failure field value")
+	}
+}
+
+// TestBatchOnConnectedFailure verifies that OnConnected
+// fails if we cannot write.
+func TestBatchOnConnectedFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
+	err := batch.OnConnected("download", "FQDN")
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
+	}
+}
+
+// TestBatchOnDownloadEvent verifies that OnDownloadEvent
+// works correctly.
+func TestBatchOnDownloadEvent(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
+	err := batch.OnDownloadEvent(&spec.Measurement{
+		BBRInfo: spec.BBRInfo{
+			MaxBandwidth: 6400000,
+			MinRTT:       71,
+		},
+		Direction: "download",
+		Elapsed:   4,
+		Origin:    "server",
+		TCPInfo: spec.TCPInfo{
+			RTTVar:      11,
+			SmoothedRTT: 150,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
+	}
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			BBRInfo struct {
+				MaxBandwidth int64   `json:"max_bandwidth"`
+				MinRTT       float64 `json:"min_rtt"`
+			} `json:"bbr_info"`
+			Direction string  `json:"direction"`
+			Elapsed   float64 `json:"elapsed"`
+			Origin    string  `json:"origin"`
+			TCPInfo   struct {
+				SmoothedRTT float64 `json:"smoothed_rtt"`
+				RTTVar      float64 `json:"rtt_var"`
+			} `json:"tcp_info"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Key != "measurement" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.BBRInfo.MaxBandwidth != 6400000 {
+		t.Fatal("Unexpected max bandwidth field value")
+	}
+	if event.Value.BBRInfo.MinRTT != 71 {
+		t.Fatal("Unexpected min rtt field value")
+	}
+	if event.Value.Direction != "download" {
+		t.Fatal("Unexpected direction field value")
+	}
+	if event.Value.Elapsed != 4.0 {
+		t.Fatal("Unexpected elapsed field value")
+	}
+	if event.Value.Origin != "server" {
+		t.Fatal("Unexpected origin field value")
+	}
+	if event.Value.TCPInfo.SmoothedRTT != 150.0 {
+		t.Fatal("Unexpected smoothed rtt field value")
+	}
+	if event.Value.TCPInfo.RTTVar != 11.0 {
+		t.Fatal("Unexpected rtt var value")
+	}
+}
+
+// TestBatchOnDownloadEventFailure verifies that OnDownloadEvent
+// fails if we cannot write.
+func TestBatchOnDownloadEventFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
+	err := batch.OnDownloadEvent(&spec.Measurement{})
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
+	}
+}
+
+// TestBatchOnUploadEvent verifies that OnUploadEvent
+// works correctly.
+func TestBatchOnUploadEvent(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
+	err := batch.OnUploadEvent(&spec.Measurement{
+		AppInfo: spec.AppInfo{
+			NumBytes: 100000000,
+		},
+		Direction: "upload",
+		Elapsed:   3.0,
+		Origin:    "client",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
+	}
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			AppInfo struct {
+				NumBytes int64 `json:"num_bytes"`
+			} `json:"app_info"`
+			Direction string  `json:"direction"`
+			Elapsed   float64 `json:"elapsed"`
+			Origin    string  `json:"origin"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Key != "measurement" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.AppInfo.NumBytes != 100000000 {
+		t.Fatal("Unexpected num bytes field value")
+	}
+	if event.Value.Direction != "upload" {
+		t.Fatal("Unexpected direction field value")
+	}
+	if event.Value.Elapsed != 3.0 {
+		t.Fatal("Unexpected elapsed field value")
+	}
+	if event.Value.Origin != "client" {
+		t.Fatal("Unexpected elapsed field value")
+	}
+}
+
+// TestBatchOnUploadEventFailure verifies that OnUploadEvent
+// fails if we cannot write.
+func TestBatchOnUploadEventFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
+	err := batch.OnUploadEvent(&spec.Measurement{
+		Elapsed: 1.0,
+	})
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
+	}
+}
+
+// TestBatchOnComplete verifies that OnComplete works correctly
+func TestBatchOnComplete(t *testing.T) {
+	sw := &mocks.SavingWriter{}
+	batch := Batch{sw}
+	err := batch.OnComplete("download")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sw.Data) != 1 {
+		t.Fatal("invalid length")
+	}
+	var event struct {
+		Key   string `json:"key"`
+		Value struct {
+			Subtest string `json:"subtest"`
+		} `json:"value"`
+	}
+	err = json.Unmarshal(sw.Data[0], &event)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if event.Key != "status.measurement_done" {
+		t.Fatal("Unexpected event key")
+	}
+	if event.Value.Subtest != "download" {
+		t.Fatal("Unexpected subtest field value")
+	}
+}
+
+// TestBatchOnCompleteFailure verifies that OnComplete
+// fails if we cannot write.
+func TestBatchOnCompleteFailure(t *testing.T) {
+	sw := &mocks.FailingWriter{}
+	batch := Batch{sw}
+	err := batch.OnComplete("download")
+	if err != mocks.ErrMocked {
+		t.Fatal("Not the error we expected")
+	}
+}
+
+// TestNewBatchConstructor verifies that we are
+// constructing an batch bound to stdout.
+func TestNewBatchConstructor(t *testing.T) {
 	batch := NewBatch()
-	err := batch.OnStarting("upload")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnConnected("upload", "FQDN")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnUploadEvent(&spec.Measurement{Elapsed: 1.0})
-	if err != nil {
-		t.Fatal("Did not expect an error here")
-	}
-	err = batch.OnComplete("upload")
-	if err != nil {
-		t.Fatal("Did not expect an error here")
+	if batch.Writer != os.Stdout {
+		t.Fatal("Batch is not using stdout")
 	}
 }
 
@@ -85,43 +346,5 @@ func TestEmitInterfaceFailure(t *testing.T) {
 		// nothing
 	default:
 		t.Fatal("Expected a json.UnsupportedTypeError here")
-	}
-}
-
-// TestEmitDataFailure ensures that any function in the batch
-// API properly deals with a writing error.
-func TestEmitDataFailure(t *testing.T) {
-	batch := Batch{mocks.FailingWriter{}}
-	err := batch.emitData([]byte("abc"))
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.emitInterface(1234)
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnStarting("download")
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnError("download", errors.New("an error"))
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnConnected("download", "server-fqdn")
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnDownloadEvent(&spec.Measurement{})
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnUploadEvent(&spec.Measurement{})
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
-	}
-	err = batch.OnComplete("download")
-	if err != mocks.ErrMocked {
-		t.Fatal("Not the result we expected")
 	}
 }
