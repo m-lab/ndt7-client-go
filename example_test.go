793a6990ac5d6f678740fe21ef017a12d1004e46
Simone Basso <bassosimone@gmail.com>
2019-07-03 15:58:09 +0200

    example_test.go: more meaningful clientName

--- example_test.go
+++ example_test.go
@@ -12,7 +12,7 @@ import (
 func Example() {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
-	client := ndt7.NewClient("example", "0.1.0")
+	client := ndt7.NewClient("ndt7-client-go-example", "0.1.0")
 	ch, err := client.StartDownload(ctx)
 	if err != nil {
 		log.Fatal(err)
