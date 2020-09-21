c1db18421d941f085234f4efbaa8c7204e980fe1
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-09-21 17:09:53 -0400

    Update docstring for new flags

--- cmd/ndt7-client/main.go
+++ cmd/ndt7-client/main.go
@@ -2,8 +2,9 @@
 //
 // Usage:
 //
-//    ndt7-client [-format <human|json>] [-hostname <name>] [-no-verify]
-//                [-scheme <scheme>] [-timeout <string>]
+//    ndt7-client [-format <human|json>] [-server <name>] [-no-verify]
+//                [-scheme <scheme>] [-timeout <string>] [-service-url <url>]
+//                [-upload <bool>] [-download <bool>]
 //
 // The `-format` flag defines how the output should be emitter. Possible
 // values are "human", which is the default, and "json", where each message
@@ -12,9 +13,17 @@
 // The (DEPRECATED) `-batch` flag is equivalent to `-format json`, and the
 // latter should be used instead.
 //
-// The `-server <name>` (previously `-hostname`) flag specifies to use the
-// `name` hostname for performing the ndt7 test. The default is to
-// auto-discover a suitable server by using Measurement Lab's locate service.
+// The default behavior is for ndt7-client to discover a suitable server using
+// Measurement Lab's locate service. This behavior may be overridden using
+// either the `-server` or `-service-url` flags.
+//
+// The `-server <name>` flag specifies the server `name` for performing
+// the ndt7 test. This option overrides `-service-url`.
+//
+// The `-service-url <url>` flag specifies a complete URL that specifies the
+// scheme (e.g. "ws"), server name and port, protocol (e.g. /ndt/v7/download),
+// and HTTP parameters. By default, upload and download measurements are run
+// automatically. The `-service-url` specifies only one measurement direction.
 //
 // The `-no-verify` flag allows to skip TLS certificate verification.
 //
@@ -26,6 +35,10 @@
 // whole test is interrupted. The `<string>` is a string suitable to
 // be passed to time.ParseDuration, e.g., "15s". The default is a large
 // enough value that should be suitable for common conditions.
+//
+// The `-upload` and `-download` flags are boolean options that default to true,
+// but may be set to false on the command line to run only upload or only
+// download.
 //
 // Additionally, passing any unrecognized flag, such as `-help`, will
 // cause ndt7-client to print a brief help message.
